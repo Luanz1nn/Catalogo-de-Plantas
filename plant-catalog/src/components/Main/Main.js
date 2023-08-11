ad203563c73e675cf9158a3a010790d49a10d0ee
Luan William <luanz1nnwill@gmail.com>
2023-08-10 22:35:42 -0300

    atualizado

---
+++ plant-catalog/src/components/Main/Main.js
@@ -0,0 +1,38 @@
+import "./style.css";
+import Header from "../Header/header";
+import Card from "../Cards/Cards";
+import species from "../Species/Species";
+import { useState } from "react";
+
+function Main() {
+  const [specie] = useState(species);
+
+  return (
+    <div className="main-container">
+      <Header />
+      <div className="search">
+      <input placeholder="Digite Algo"></input>
+      <button>Buscar</button>
+      </div>
+      
+      {specie.map((specieCurrent) => {
+        return (
+          <Card
+            key={specieCurrent.id}
+            Nomecientifíco={specieCurrent.Nomecientifíco}
+            Nomecomum={specieCurrent.Nomecomum}
+            imagemcapa={specieCurrent.imagemcapa}
+            Ocorrência={specieCurrent.Ocorrência}
+            Porte={specieCurrent.Porte}
+            Frutificação={specieCurrent.Frutificação}
+            Floração={specieCurrent.Floração}
+            Disperção={specieCurrent.Disperção}
+            Estado={specieCurrent.Estado}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Main;
